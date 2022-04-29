4da567b8d22ca48187239c2c4b7e35f3434d5cf8
AlmeenT <tiamiyualameen@ymail.com>
2022-04-29 04:47:06 -0700

    routing

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def index():
-    return "Hello World"
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/<firstname>/<lastname>')
+def greet(firstname,lastname):
+    name = f'{firstname}{lastname}'
+    return render_template('greet.html', name= name)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
