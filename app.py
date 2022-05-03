11f3e8e219bd6ef1a764f0f1c8b81a227c826edb
AlmeenT <tiamiyualameen@ymail.com>
2022-05-03 10:35:12 -0700

    added and connected bootstrap

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/about')
+@app.route('/dashboard')
 def about():
-    return render_template('about.html')
+    return render_template('dashboard.html')
 
-@app.route('/<firstname>/<lastname>')
-def greet(firstname,lastname):
-    name = f'{firstname}{lastname}'
-    return render_template('greet.html', name= name)
 
 if __name__ == "__main__":
     app.run(debug=True)
